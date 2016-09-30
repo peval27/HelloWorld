ace18d0ad55cb8a2a87db6424c29b828eddb9b46
paolo <peval27@gmail.com>
2016-09-30 15:16:06 +0100

    Added OS description in Command Line output
    Tested only on Win Server 2012

--- src/HelloWorld/Program.cs
+++ src/HelloWorld/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace HelloWorld
 {
     public class Program
     {
         public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
+        {            
+            Console.WriteLine($"Hello World from {RuntimeInformation.OSDescription}");
         }
     }
 }
